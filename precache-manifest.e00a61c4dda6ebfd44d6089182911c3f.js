46def904142c630cc0a8fdb6bab4681a5a51c72d
Austin Greene <agreene@coursera.org>
2020-04-19 18:19:00 -0700

    Updates

---
+++ precache-manifest.e00a61c4dda6ebfd44d6089182911c3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5bf193d43870709254317241fe41ee9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58afaa601f823c085aa3",
+    "url": "/static/css/main.969208b0.chunk.css"
+  },
+  {
+    "revision": "7a2cee81a0e1b041b5a3",
+    "url": "/static/js/2.67107b2e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.67107b2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58afaa601f823c085aa3",
+    "url": "/static/js/main.311861e5.chunk.js"
+  },
+  {
+    "revision": "9506785a349cde6253b3",
+    "url": "/static/js/runtime-main.0400e441.js"
+  }
+]);
